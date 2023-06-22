8450a433dbff1fa7fe62d3c71777676ed6ab7065
HenningCV <65901747+HenningCV@users.noreply.github.com>
2023-06-22 20:11:54 +0200

    adding minor hit detection

---
+++ Assets/Scripts/sword/Weak_side_hit.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weak_side_hit : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("enemy_fence"))
+        {
+            print("Weak side got hit!");
+        }       
+    }
+}
