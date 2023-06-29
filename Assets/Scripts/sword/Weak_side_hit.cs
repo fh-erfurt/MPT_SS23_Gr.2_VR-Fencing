ad426311d2d7d15965992fdf6952ec1666d58e9e
HenningCV <henningvenhorst@gmail.com>
2023-06-29 13:11:33 +0200

    added point gain

--- Assets/Scripts/sword/Weak_side_hit.cs
+++ Assets/Scripts/sword/Weak_side_hit.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 public class Weak_side_hit : MonoBehaviour
 {
+    private float pointGain;
+    public float points = 100;
+
+    private void Start()
+    {
+        pointGain = points;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        if(other.CompareTag("enemy_fence"))
+        if (other.CompareTag("enemy_fence"))
         {
             print("Weak side got hit!");
-        }       
+            Points.AddPoints(pointGain);
+            print("You gained:" + pointGain + " points");
+        }
     }
 }
